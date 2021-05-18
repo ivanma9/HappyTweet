9343deaae1fd738abbce1457736a1a92dc50e6d8
Ivan <ivanma819@gmail.com>
2021-05-17 20:23:54 -0700

    Fix small errors

--- Group Assignments/Week7 /js/map.js
+++ Group Assignments/Week7 /js/map.js
@@ -190,7 +190,7 @@ function createInfoPanel(){
 		// if feature is not highlighted
 		else
 		{
-			this._div.innerHTML = 'Hover over a country';
+			this._div.innerHTML = 'Hover over a state';
 		}
 	};
 
