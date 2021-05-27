3e8fc61a563d3613c4a507fc76e0feb97de2b25a
Ivan <ivanma819@gmail.com>
2021-05-27 16:15:35 -0700

    add slider

--- Group Assignments/Week9/js/slider.js
+++ Group Assignments/Week9/js/slider.js
@@ -1,8 +1,22 @@
-const range = document.querySelector('input');
-const div = document.querySelector('.moji');
-const mojis = ['😪','🥺','😩','😞','🙁','😐','🙂','😀','😄','😎','🤩'];
+document.addEventListener("DOMContentLoaded", function () {
+	const range = document.getElementById("happySlider");
+	const div = document.querySelector(".moji");
+	const mojis = [
+		"😪",
+		"🥺",
+		"😩",
+		"😞",
+		"🙁",
+		"😐",
+		"🙂",
+		"😀",
+		"😄",
+		"😎",
+		"🤩",
+	];
 
-range.addEventListener('input', (e) => {
-  let rangeValue = e.target.value;
-  div.textContent = mojis[rangeValue];
+	range.addEventListener("input", (e) => {
+		let rangeValue = e.target.value;
+		div.textContent = mojis[rangeValue];
+	});
 });
