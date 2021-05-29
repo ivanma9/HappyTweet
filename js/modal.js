16a5532ba415d3013da89ed23ad8b2a03c9834a6
Ivan <ivanma819@gmail.com>
2021-05-29 15:26:28 -0700

    add css

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,6 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
 	modal.style.display = "block";
-	console.log("CLIK");
 };
 
 // When the user clicks on <span> (x), close the modal
